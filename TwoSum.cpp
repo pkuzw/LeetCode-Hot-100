cf332a1901d7eca47fa0c31c1c505052a4d8e5bf
pkuzw <zhaowei.pku@gmail.com>
2021-01-31 21:24:43 +0800

    LeetCode Hot 100, Problem 1. TwoSum

---
+++ TwoSum.cpp
@@ -0,0 +1,20 @@
+//
+// Created by zhaowei01 on 2021/1/31.
+//
+
+#include "TwoSum.h"
+
+vector<int> Solution::twoSum(vector<int>& nums, int target) {
+    vector<int> result = {};
+    for (int i = 0; i < nums.size() - 1; ++i) {
+        for (int j = i + 1; j < nums.size(); ++j) {
+            if (nums.at(i) + nums.at(j) == target) {
+                result.push_back(i);
+                result.push_back(j);
+                return result;
+            }
+        }
+    }
+    return result;
+}
+
---
+++ TwoSum.h
@@ -0,0 +1,19 @@
+//
+// Created by zhaowei01 on 2021/1/31.
+//
+
+#ifndef LEETCODE_HOT_100_TWOSUM_H
+#define LEETCODE_HOT_100_TWOSUM_H
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target);
+
+};
+
+
+#endif //LEETCODE_HOT_100_TWOSUM_H
