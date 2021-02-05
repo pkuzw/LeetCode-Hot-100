0846d89288b03191bb5f9ad27280d9b57597edee
pkuzw <zhaowei.pku@gmail.com>
2021-02-05 23:04:37 +0800

    LeetCode-Hot-100, Problem 6. Container With Most Water, C++. Deal with double pointer from both sides to squeeze inside.

--- ContainerWithMostWater.h
+++ ContainerWithMostWater.h
@@ -12,24 +12,6 @@ using namespace std;
 class ContainerWithMostWater {
 public:
     int maxArea(vector<int>& height);
-    int maxArea(vector<int>& height) {
-        if (height.empty()) {
-            return 0;
-        }
-        int max_area = 0;
-        vector<int>::size_type l = 0, r = height.size() - 1;
-        while (l < r) {
-            int cur_area = (r - l) * min(height[l], height[r]);
-            max_area = max_area < cur_area ? cur_area : max_area;
-            if (height[l] <= height[r]) {
-                ++l;
-            } else {
-                --r;
-            }
-        }
-        return max_area;
-    }
-
 };
 
 
