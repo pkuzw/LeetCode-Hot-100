c70f67e6b735c1a288117f45e245b3214f413c74
pkuzw <zhaowei.pku@gmail.com>
2021-02-10 00:12:51 +0800

    LeetCode-Hot-100, Problem 21. Merge Two Sorted List, C++. Space O(1), debugging...

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -11,18 +11,19 @@ ListNode* MergeTwoSortedLists::mergeTwoLists(ListNode* l1, ListNode* l2) {
     if (!l2) {
         return l1;
     }
-    ListNode* iter1 = l1, *iter2 = l2;
+    ListNode* iter1_pre = new ListNode(0, l1), *iter2_pre = new ListNode(0, l2);
+    ListNode* iter1 = iter1_pre, *iter2 = iter2_pre;
     ListNode* iter1_nxt = l1->next, *iter2_nxt = l2->next;
     while (iter1->next && iter2->next) {
         if (iter1_nxt->val < iter2_nxt->val) {
-            if (iter2->val < iter1->val) {
+            if (iter2->val <= iter1->val) {
                 iter2->next = iter1;
             }
             iter2 = iter2_nxt;
             iter1 = iter1->next;
             iter1_nxt = iter1->next;
         } else {
-            if (iter1->val < iter2->val) {
+            if (iter1->val <= iter2->val) {
                 iter1->next = iter2;
             }
             iter1 = iter1_nxt;
@@ -35,9 +36,10 @@ ListNode* MergeTwoSortedLists::mergeTwoLists(ListNode* l1, ListNode* l2) {
         iter2->next = iter1;
         return l1->val < l2->val ? l1 : l2;
     }
-    if (iter2->next && iter1) {
+    if (iter1->val <= iter2->val) {
         iter1->next = iter2;
-        return l1->val < l2->val ? l1 : l2;
+    } else {
+        iter2->next = iter1;
     }
     return l1->val < l2->val ? l1 : l2;
 }
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main() {
-    ListNode l1(15), l2(13, &l1), l3(10, &l2), l4(9, &l3), l5(1, &l4);
-    ListNode r1(12), r2(11, &r1), r3(7, &r2), r4(6, &r3), r5(4, &r4);
+    ListNode l1(10), l2(7, &l1), l3(4, &l2);
+    ListNode r1(11), r2(10, &r1), r3(8, &r2), r4(6, &r3), r5(4, &r4);
     MergeTwoSortedLists mts;
-    ListNode* rslt = mts.mergeTwoLists(&l5, &r5);
+    ListNode* rslt = mts.mergeTwoLists(&l3, &r5);
     while (rslt) {
         cout << rslt->val << ' ';
         rslt = rslt->next;
