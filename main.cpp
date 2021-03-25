4834c0b799ea7c9f42daafa5a415b012253f0da1
pkuzw <zhaowei.pku@gmail.com>
2021-03-25 23:35:27 +0800

    LeetCode-Hot-100, Problem 34. Find First and Last Position of Element in Sorted Array, C++. Binary Search. Use self-defined binary search function.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using std::endl;
 
 int main() {
     FindRangeOfTargetInSortedArray fr;
-    vector<int> nums = {};
-    auto range = fr.searchRange(nums, 8);
+    vector<int> nums = {1, 2, 3, 4};
+    auto range = fr.searchRange(nums, 3);
     cout << range[0] << ' ' << range[1] << endl;
     return 0;
 }
