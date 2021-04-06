210531b08464ec3964acdb8eff7df29699bdae9f
pkuzw <zhaowei.pku@gmail.com>
2021-04-06 22:11:48 +0800

    LeetCode Hot 100, Problem 42, Trap Rain Water, C++. DP, coding.

---
+++ TrapRainWater.cpp
@@ -0,0 +1,9 @@
+//
+// Created by zhaowei01 on 2021/4/6.
+//
+
+#include "TrapRainWater.h"
+
+int TrapRainWater::trap(vector<int> &height) {
+
+}
---
+++ TrapRainWater.h
@@ -0,0 +1,18 @@
+//
+// Created by zhaowei01 on 2021/4/6.
+//
+
+#ifndef LEETCODE_HOT_100_TRAPRAINWATER_H
+#define LEETCODE_HOT_100_TRAPRAINWATER_H
+
+#include <vector>
+
+using std::vector;
+
+class TrapRainWater {
+public:
+    int trap(vector<int>& height);
+};
+
+
+#endif //LEETCODE_HOT_100_TRAPRAINWATER_H
