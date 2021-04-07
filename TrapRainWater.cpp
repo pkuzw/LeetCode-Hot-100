66abf7589559b59c06e41d585f6e9f19b63d63d5
pkuzw <zhaowei.pku@gmail.com>
2021-04-07 23:31:01 +0800

    LeetCode-Hot-100, Problem 42. Trap Rain Water, C++. Dp.

--- TrapRainWater.cpp
+++ TrapRainWater.cpp
@@ -1,9 +1,37 @@
 //
 // Created by zhaowei01 on 2021/4/6.
 //
+#include <algorithm>
 
 #include "TrapRainWater.h"
 
-int TrapRainWater::trap(vector<int> &height) {
+using std::min;
 
+int TrapRainWater::trap(vector<int> &height) {
+    if (height.size() < 3) {
+        return 0;
+    }
+    const auto& n = height.size();
+    vector<int> left_max(n, 0), right_max(n, 0);
+    left_max[0] = height[0];
+    right_max[n-1] = height[n-1];
+    for (int i = 1; i != n; ++i) {
+        if (height[i] > left_max[i-1]) {
+            left_max[i] = height[i];
+        } else {
+            left_max[i] = left_max[i-1];
+        }
+    }
+    for (int i = n - 2; i >= 0; --i) { //此处不可以使用auto，会导致下溢死循环
+        if (height[i] > right_max[i+1]) {
+            right_max[i] = height[i];
+        } else {
+            right_max[i] = right_max[i+1];
+        }
+    }
+    int total_water = 0;
+    for (auto i = 1; i != n-1; ++i) {
+        total_water += min(left_max[i], right_max[i]) - height[i];
+    }
+    return total_water;
 }
--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include "CombinationSum.h"
+#include "TrapRainWater.h"
 using std::cout;
 using std::endl;
 
 int main() {
-    CombinationSum cs;
-    vector<int> nums = {2};
-    int target = 7;
-    vector<vector<int>> result = cs.combinationSum(nums, target);
-    for (auto& i : result) {
-        for (auto & j : i) {
-            cout << j << ' ';
-        }
-        cout << endl;
-    }
+    TrapRainWater trw;
+    vector<int> nums = {4,2,0,3,2,5};
+    cout << trw.trap(nums) << endl;
     return 0;
 }
